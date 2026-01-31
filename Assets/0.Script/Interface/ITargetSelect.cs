0c43d6b28718831271f4cd991fd58319937a7dfd
a01064946264-ui <a01064946264@gmail.com>
2026-02-01 00:22:16 +0900

    [Fix] TargetSelectArrow Edit
    타겟 선택 화살표 타겟 UI 위치로 세팅되도록 수정

--- Assets/0.Script/Interface/ITargetSelect.cs
+++ Assets/0.Script/Interface/ITargetSelect.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 public interface ITargetSelect
 {
     event Action<BattleInfo> OnTargetSelected;
+    event Action<BattleInfo> OnResetTargetSelected;
     
     public Dictionary<string, BattleInfo> BattleSequence { get; }
     
